c242ab061c020caa52b7a43d0bf569536a9406a9
Oleksandr Ukrainets <ukrainets.alexander@gmail.com>
2025-04-30 21:16:58 +0200

    IAccessRequestRepository created

---
+++ Strongbox.Domain/Interfaces/IAccessRequestRepository.cs
@@ -0,0 +1,13 @@
+﻿using Strongbox.Domain.Entities;
+
+namespace Strongbox.Domain.Interfaces
+{
+    public interface IAccessRequestRepository
+    {
+        public Task<Guid> CreateAccessRequest(AccessRequest accessRequest);
+        public Task<AccessRequest> GetAccessRequest(Guid accessRequestId);
+        public Task<AccessRequest> UpdateAccessRequest(AccessRequest accessRequest);
+        public Task<bool> DeleteAccessRequest(Guid accessRequestId);
+        public Task<ICollection<AccessRequest>> GetAccessRequests();
+    }
+}
